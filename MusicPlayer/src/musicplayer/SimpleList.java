3fd82c0f022ccc6abbe5a97bc336ae06f5264753
jmo.py <jafet.moraugalde15@gmail.com>
2021-03-29 22:03:59 -0600

    Create the class for the simple linked list.

---
+++ MusicPlayer/src/musicplayer/SimpleList.java
@@ -0,0 +1,24 @@
+package musicplayer;
+
+public class SimpleList {
+    SongNode head;
+    
+    public boolean isEmpty(){
+        return (head == null);
+    }
+    
+    public void add(SongNode newSong){
+        if(!isEmpty()){
+            SongNode temp = head;
+            
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newSong;   
+        }
+    }
+    
+    public void remove(){
+        
+    }
+}
