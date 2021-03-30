8ee35b9ca4bdb7379fbf5c16f421249bde88f03b
jmo.py <jafet.moraugalde15@gmail.com>
2021-03-29 22:02:31 -0600

    Create a GUI for the library.

---
+++ MusicPlayer/src/musicplayer/LibraryGUI.java
@@ -0,0 +1,261 @@
+package musicplayer;
+
+public class LibraryGUI extends javax.swing.JFrame {
+    public LibraryGUI() {
+        initComponents();
+        this.setTitle("Library");
+        this.setLocationRelativeTo(null);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txtSingerName = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblInformacion = new javax.swing.JTable();
+        txtSongName = new javax.swing.JTextField();
+        txtSongDuration = new javax.swing.JTextField();
+        btnExit = new javax.swing.JButton();
+        cbxAños = new javax.swing.JComboBox<>();
+        lblLibrary = new javax.swing.JLabel();
+        btnCreatePlayList = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
+        btnPlay = new javax.swing.JButton();
+        btnNext = new javax.swing.JButton();
+        btnPrev = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txtSingerName.setText("Singer name");
+        txtSingerName.setName("txtSingerName"); // NOI18N
+        txtSingerName.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtSingerNameMouseClicked(evt);
+            }
+        });
+
+        tblInformacion.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Cantante", "Cancion", "Duracion", "Año"
+            }
+        ));
+        tblInformacion.setCellSelectionEnabled(true);
+        jScrollPane1.setViewportView(tblInformacion);
+        tblInformacion.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+
+        txtSongName.setText("Song name");
+        txtSongName.setName("txtSongName"); // NOI18N
+        txtSongName.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtSongNameMouseClicked(evt);
+            }
+        });
+        txtSongName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtSongNameActionPerformed(evt);
+            }
+        });
+
+        txtSongDuration.setText("Duration of the song");
+        txtSongDuration.setName("txtSongDuration"); // NOI18N
+        txtSongDuration.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtSongDurationMouseClicked(evt);
+            }
+        });
+
+        btnExit.setText("Exit");
+        btnExit.setName("btnExit"); // NOI18N
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+
+        cbxAños.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021" }));
+        cbxAños.setName("cbxAños"); // NOI18N
+
+        lblLibrary.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lblLibrary.setText("Library");
+        lblLibrary.setName("lblLibrary"); // NOI18N
+
+        btnCreatePlayList.setText("Playlist + ");
+        btnCreatePlayList.setName("btnCreatePlayList"); // NOI18N
+        btnCreatePlayList.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCreatePlayListActionPerformed(evt);
+            }
+        });
+
+        btnAdd.setText("Add");
+        btnAdd.setName("btnAdd"); // NOI18N
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+
+        btnPlay.setText("Play/Pause");
+        btnPlay.setName("btnPlay"); // NOI18N
+        btnPlay.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPlayActionPerformed(evt);
+            }
+        });
+
+        btnNext.setText("Next");
+        btnNext.setName("btnNext"); // NOI18N
+        btnNext.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNextActionPerformed(evt);
+            }
+        });
+
+        btnPrev.setText("Prev");
+        btnPrev.setName("btnPrev"); // NOI18N
+        btnPrev.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrevActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(btnCreatePlayList)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnExit))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(155, 155, 155)
+                                .addComponent(lblLibrary))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(87, 87, 87)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtSongName)
+                                    .addComponent(txtSingerName)
+                                    .addComponent(txtSongDuration, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(cbxAños, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(btnAdd))))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnPrev)
+                .addGap(18, 18, 18)
+                .addComponent(btnPlay)
+                .addGap(18, 18, 18)
+                .addComponent(btnNext)
+                .addGap(73, 73, 73))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(lblLibrary)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtSingerName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cbxAños, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtSongName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtSongDuration, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAdd))
+                .addGap(40, 40, 40)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnPlay)
+                    .addComponent(btnNext)
+                    .addComponent(btnPrev))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCreatePlayList)
+                    .addComponent(btnExit))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        this.dispose();
+        new HomeScreenGUI().setVisible(true);
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    private void btnCreatePlayListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCreatePlayListActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnCreatePlayListActionPerformed
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnPlayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPlayActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPlayActionPerformed
+
+    private void btnNextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnNextActionPerformed
+
+    private void btnPrevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrevActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPrevActionPerformed
+
+    private void txtSingerNameMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtSingerNameMouseClicked
+        txtSingerName.setText("");
+    }//GEN-LAST:event_txtSingerNameMouseClicked
+
+    private void txtSongNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSongNameActionPerformed
+        
+    }//GEN-LAST:event_txtSongNameActionPerformed
+
+    private void txtSongNameMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtSongNameMouseClicked
+        txtSongName.setText("");
+    }//GEN-LAST:event_txtSongNameMouseClicked
+
+    private void txtSongDurationMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtSongDurationMouseClicked
+        txtSongDuration.setText("");
+    }//GEN-LAST:event_txtSongDurationMouseClicked
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LibraryGUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnCreatePlayList;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnNext;
+    private javax.swing.JButton btnPlay;
+    private javax.swing.JButton btnPrev;
+    private javax.swing.JComboBox<String> cbxAños;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblLibrary;
+    private javax.swing.JTable tblInformacion;
+    private javax.swing.JTextField txtSingerName;
+    private javax.swing.JTextField txtSongDuration;
+    private javax.swing.JTextField txtSongName;
+    // End of variables declaration//GEN-END:variables
+}
